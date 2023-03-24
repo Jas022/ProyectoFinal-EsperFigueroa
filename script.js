41b9888ccad2dde95eb21bbb97d827cfe4ea5a6a
Jasmin Esper <jasmin.09-@live.com.ar>
2023-03-23 21:29:15 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,121 +1,153 @@
+// Bienvenida del usuario
+
+alert("¡Bienvenido, a ArtesaniasSte!")
+
+class Producto {
+    constructor (codigo, nombre, precio, cantidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad= cantidad
+    }
+}
+
+const producto1 = new Producto(001,"Macetas chicas", 300,0)
+const producto2 = new Producto(002,"Macetas medianas", 500,0)
+const producto3 = new Producto(003,"Macetas grandes", 700,0)
+
+
+
+// array vacios con sus push
+
+const productos = []
+
+productos.push(producto1)
+productos.push(producto2)
+productos.push(producto3)
+
+
+// funciones
+
+const buscarObjeto = (array, busqueda) => {return array.find(productos => productos.nombre === busqueda)}
+const sumarCompras = (n1, n2, n3, n4) => {return resultado = n1 + n2 + n3 + n4}
+
+
+// Funcion limpiar
+
 function limpiar() {
+
     let opcion = confirm("¿Estas seguro? Se borrará el contenido de su carrito");
     
     if (opcion) {
-        cantmacetas = 0;
-        total = 0;
         console.clear();
         console.log("\n\n El carrito se ha vaciado! Ya podes volver a comprar \n\n");
-    } else {
-        console.log("\n\n El carrito no cambio, podes seguir con tu compra \n\n");
-    }
-    }
-    
-    function mostrar() {
-    console.log(
-        "\n\n Tu carrito lleva en total " +
-        cantmacetas +
-        " macetas. \n Tenes " +
-        macetaschicas  +
-        " macetaschicas; " +
-        macetasmedianas +
-        " macetasmedianas, y " +
-        macetasgrandes +
-        " macetas grandes.\n El total gastado hasta el momento es de $" +
-        total + "\n\n");
-    }
-    
-    let menu,
-    menu2,
-    total = 0,
-    cantmacetas = 0,
-    macetaschicas = 0,
-    macetasgrandes = 0,
-    macetasmedianas = 0;
-    
-    do {
-    menu = Number(prompt(
-        "- Ingrese el numero, segun lo que desea hacer: \n \n \
-            1) Comprar. \n \
-            2) Carrito. \n \
-            3) Reinicia. \n \
-            0) Salir"
-        ));
-    
-    switch (menu) {
-        case 1:
-        do {
-            menu2 = Number(
-            prompt(
-                "- Elija el producto que desea comprar:\n\n\
-                    1) Macetas chicas ($300) \n \
-                    2) Macetas medianas ($500) \n \
-                    3) Macetas grandes ($750) \n \
-                    4) Ver total \n \
-                    0) Volver al menu anterior"
-              
-                    
-            )
-            );
-            switch (menu2) {
-            case 1:
-                total = total + 300;
-                cantmacetas++;
-                macetaschicas ++;
-                console.log("\n Agregaste una Maceta chica al carrito por un valor de $300", total);
-                break;
-            case 2:
-                total = total + 500;
-                cantmacetas++;
-                macetasmedianas++;
-                console.log("\n Agregaste una Maceta mediana al carrito $500", total);
-                break;
-            case 3:
-                total = total + 750;
-                cantmacetas++;
-                macetasgrandes++;
-                console.log("\n Agregaste una Maceta grandes al carrito $750", total);
-                break;
-            case 4:
-                total = total
-                console.log("\n El total de tu compra es",total)
-                break;
-            case 5:
-                menu2 = menu2
-            console.log("\n Volver al menu");
-                break;
-        
-            default:
-                console.log("\n Numero invalido, porfavor ingresa nuevamente la opcion \n");
-                break;
+        } 
+            else {
+            console.log("\n\n El carrito no cambio, podes seguir con tu compra \n\n");
             }
-        } while (menu2 != 0);
-        break;
+}
     
-        case 2:
-            mostrar();
-        break;
-        case 3:
+// Funcion Mostrar
+
+// function filtradoCarrito(){
+//     for(i =0; i < productos.length; i++){
+//         if(productos[i].cantidad > 0){
+//            console.log(`productos:${productos[i].nombre}, cantidad: ${productos[i].cantidad}`)
+//         }
+//     }
+// }
+
+function mostrar(nombre, cantidad) {
+     
+     console.log(`-producto:${nombre}, cantidad:${cantidad}`)
+        
+}
+
+function ofertas(){
+    let filter = productos.filter(producto=> producto.precio <= 500)
+     for (i=0; i< filter.length; i++){
+        console.log(`- producto: ${productos[i].nombre}, -precio: ${productos[i].precio}`)
+     }
+}
+
+let menu, menu2, total=0
+
+do{
+        menu = Number(prompt(
+            "- Ingrese el numero, segun lo que desea hacer: \n \n \
+                1) Comprar. \n \
+                2) Carrito. \n \
+                3) Ofertas. \n \
+                4) Reiniciar Compra. \n \
+                0) Salir" ));
+        switch (menu) {
+            case 1:  do{
+
+                menu2 = Number(
+                    prompt(
+                        `- Elija el producto que desea comprar:\n\n\
+                        1) ${producto1.nombre} ($${producto1.precio}) \n \
+                        2) ${producto2.nombre} ($${producto2.precio}) \n \
+                        3) ${producto3.nombre} ($${producto3.precio}) \n \
+                        4) Ver total \n \
+                        0) Volver al menu anterior
+                        `));
+                switch(menu2){
+                    case 1: let cantProdUno = Number(prompt(
+                            "Ingrese la cantidad"
+                            ))
+                            producto1.cantidad = cantProdUno
+                            let precioTotalProd = producto1.cantidad * producto1.precio
+                            total= total+ precioTotalProd
+                            console.log(`Agregaste ${producto1.cantidad} ${producto1.nombre} por un precio de ${producto1.precio} c/u, subtotal: $${precioTotalProd}`)
+                    break;
+                    case 2: let cantProdDos = Number(prompt(
+                                "Ingrese la cantidad"
+                            ))
+                            producto2.cantidad = cantProdDos
+                            let precioTotalProdDos = producto2.cantidad * producto2.precio
+                            total= total+ precioTotalProdDos
+                            console.log(`Agregaste ${producto2.cantidad} ${producto2.nombre} por un precio de ${producto2.precio} c/u, subtotal: $${precioTotalProdDos}`)
+                        break;
+                    case 3: let cantProdTres = Number(prompt(
+                                    "Ingrese la cantidad"
+                                ))
+                            producto3.cantidad = cantProdTres
+                            let precioTotalProdTres = producto3.cantidad * producto3.precio
+                            total= total+ precioTotalProdTres
+                            console.log(`Agregaste ${producto3.cantidad} ${producto3.nombre} por un precio de ${producto3.precio} c/u, subtotal: $${precioTotalProdTres}`)
+                        break;
+                    case 4: console.log(
+                                `El valor total de tu compra es: ${total}`
+                            )
+                    default:
+                        break;
+                }
+            }while(menu2 !==0)
+                
+                break;
+            case 2: 
+                console.log('Detalle de carrito de compras')
+                for(i =0; i < productos.length; i++){
+                    if(productos[i].cantidad > 0){
+                   
+                     mostrar(productos[i].nombre,productos[i].cantidad)
+                    }
+                }
+                console.log(`total: ${total}`)
+            break;
+            case 3:
+                console.log('Las Ofertas del Día son las siguientes:') 
+                //agregar ofertas del mes
+                ofertas()
+                break;
+            case 4: 
             limpiar();
-        break;
-        case 0:
-        case 5:
-        break;
-        default:
-            console.log("\n Por favor ingresa una opcion valida \n");
-        break;
-      }
-    } while (menu != 0);
-    
-    console.log(
-        "\n\n\n Desde Artesanias Ste te agradecemos por tu compra! \n Tu carrito final tiene un total " +
-        cantmacetas +
-        " macetas. \n Vas a llevar " +
-        macetaschicas +
-        " macetaschicas; " +
-        macetasmedianas +
-        " macetasmedianas, y " +
-        macetasgrandes +
-        " macetasgrandes.\n El total de la compra es $" +
-        total + 
-        ".\n agregar medio de pago");
+                    console.log(' reiniciar compras')
+                //hacer do while 2 opciones
+                break; 
+            default:
+                break;
+        }
+
+} while(menu !== 0) alert ("¡Gracias por tu compra!")
